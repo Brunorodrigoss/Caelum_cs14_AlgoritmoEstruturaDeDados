17c5720660c024b4286e511437488553567f245e
Bruno Rodrigo <bnorodrigo@gmail.com>
2017-09-14 05:05:30 -0300

    Criação do arquivo

---
+++ ed/src/br/com/caelum/ed/Vetor.java
@@ -0,0 +1,34 @@
+package br.com.caelum.ed;
+
+public class Vetor {
+	
+	private Aluno [] alunos = new Aluno[100];
+	
+	public void adiciona(Aluno aluno){
+		//implementação
+	}
+	
+	public void adiciona(int posicao, Aluno aluno){
+		//implementação
+	}
+	
+	public Aluno pega(int posicao){
+		// implementação
+	}
+	
+	public void remove(int posicao){
+		//implementação
+	}
+	
+	public boolean contem(Aluno aluno){
+		//implementação
+	}
+	
+	public int tamanho(){
+		//implementação
+	}
+	
+	public String toString(){
+		return Arrays.toString(alunos);
+	}
+}
